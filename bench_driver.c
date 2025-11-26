f27f852dd73a9e99298f083f8f5ae2832d3943aa
Vinayak Dev <vinayakdev.sci@gmail.com>
2025-11-26 06:06:38 +0000

    [Benchmarking] Allow calling benchmark functions to pass in name pointer

--- bench_driver.c
+++ bench_driver.c
@@ -1,10 +1,11 @@
 #include "devblas/c_api/blas.h"
 #include <assert.h>
+#include <stdio.h>
 
 int main(void) {
-  bench_igemm(naive_igemm_ijk, 3, DEVBLAS_LAYOUT_ROW_MAJOR, 1024, 1024, 1024,
-              1024, 1024, 1024);
-  bench_sgemm(naive_sgemm_ijk, 3, DEVBLAS_LAYOUT_COLUMN_MAJOR, 1024, 1024, 1024,
-              1024, 1024, 1024);
+  bench_igemm(naive_igemm_ijk, "naive_gemm_ijk_driver", 3,
+              DEVBLAS_LAYOUT_ROW_MAJOR, 1024, 1024, 1024, 1024, 1024, 1024);
+  bench_sgemm(naive_sgemm_ijk, "naive_gemm_ijk_driver", 3,
+              DEVBLAS_LAYOUT_COLUMN_MAJOR, 1024, 1024, 1024, 1024, 1024, 1024);
   return 0;
 }
