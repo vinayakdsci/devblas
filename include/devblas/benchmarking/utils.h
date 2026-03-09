616d806f34b3e451bb62d1226a439095355f983f
Vinayak Dev <vinayakdev.sci@gmail.com>
2026-03-10 00:21:17 +0530

    [Utils] Improve Timer clock resolution

--- include/devblas/benchmarking/utils.h
+++ include/devblas/benchmarking/utils.h
@@ -9,8 +9,8 @@ struct Timer {
   Timer() : start_(std::chrono::steady_clock::now()) {}
   double elapsed_ms() {
     auto now = std::chrono::steady_clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(now - start_)
-        .count();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(now - start_)
+        .count() * 1e-6;
   }
   double elapsed_s() { return elapsed_ms() * (double)1e-3; }
   void reset() { start_ = std::chrono::steady_clock::now(); }
