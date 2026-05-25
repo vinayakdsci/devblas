c2ccc5650f2a8c68a667e3720a1946cf8d5d44af
Vinayak Dev <vinayakdev.sci@gmail.com>
2026-05-26 00:10:12 +0530

    [devblas] Rework architecture to support microkernels
    The journey begins!

--- bench_driver.c
+++ bench_driver.c
@@ -1,20 +1,6 @@
-#include "devblas/c_api/blas.h"
+#include <devblas/devblas.h>
 #include <assert.h>
-#include <stdio.h>
 
-#define SQ (1024)
 int main(void) {
-    devblas_gemm_config_t config = {
-        .layout = DEVBLAS_LAYOUT_ROW_MAJOR,
-        .M = SQ,
-        .N = SQ,
-        .K = SQ,
-        .lda = SQ + 8,
-        .ldb = SQ + 8,
-        .ldc = SQ + 8,
-        .tileSize = 16,
-    };
-
-    bench_sgemm(tiled_sgemm, "my_exp_kernel", 2, 5, &config);
     return 0;
 }
---
+++ core/src/utils.c
@@ -0,0 +1,1 @@
+#include <devblas/api/utils.h>
